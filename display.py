06df4c855a216f0f94e9b33d6b95936784d2f1d4
renanbb5 <165321001+renanbb5@users.noreply.github.com>
2024-04-10 16:00:45 -0300

    Add files via upload

---
+++ display.py
@@ -0,0 +1,17 @@
+#date:04-10-24
+#this code its about displaying output for a basic tic-tac-toe 
+
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
+
+row2[1] = 'X'
+
+display(row1,row2,row3)
+
+

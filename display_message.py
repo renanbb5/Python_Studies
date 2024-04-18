c1008c260f78eb183b207ecf1cdd448632c8d883
Renan <165321001+renanbb5@users.noreply.github.com>
2024-04-17 21:53:33 -0300

    Add files via upload

---
+++ display_message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print('In this chapte I m learnning about how fuctions works')
+
+display_message()
---
+++ favoritebook.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    print(f'Um dos meus livros favoritos é : {title.upper()}!')
+
+favorite_book('alice no país das maravilhas')
+
